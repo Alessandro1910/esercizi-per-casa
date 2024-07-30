edae584f144b84e94c07b1ec1e6a3e8db0821742
Alessandro1910 <alessandroturacci1910@gmail.com>
2024-07-30 12:55:39 +0200

    first lesson javascript

---
+++ javascript/index.js
@@ -0,0 +1,88 @@
+test = 1;
+
+var index = 2;
+
+
+let name = "alessandro";
+
+const age = 33;
+
+name = "mattia";
+
+const isOk = false;
+
+const colors = ["red", "blue", "green"];
+
+const user = {
+    name: "ana",
+    age: 30,
+    email: "ana@gmail.com",
+};
+
+colors[1];
+
+console.log(colors[0]);
+
+console.log(user.email);
+
+console.log(user["email"]);
+
+const keyName = "age";
+
+console.log(user[keyName]);
+
+const users = [
+    {
+        name: "alessandro",
+        email: "ale@gmail.com"
+    },
+    {
+        name: "mattia",
+        email: "mattia@gmail.com"
+    },
+    {
+        name: "marco",
+        email: "marco@gmail.com"
+    }
+];
+
+console.log(users[1].email);
+
+console.log(users[0].email);
+
+console.log(users[2].email);
+
+console.log(users[2].name);
+
+users[1].email = "mattia@hotmail.com";
+
+console.log(users[1].email);
+
+console.log(colors);
+colors[0] = "gray";
+
+console.log(colors);
+
+console.log(users);
+
+users[2] = 10;
+
+console.log(users);
+
+console.log(user);
+
+user.name = "giacomo";
+
+console.log(user);
+
+let counter = 0;
+
+console.log(counter);
+
+counter = counter + 1;
+
+console.log(counter);
+
+const test1 = undefined;
+
+const test2 = null;
