1eac2508298713fed0896f0f78ef10dc2b8e9d49
Alessandro1910 <alessandroturacci1910@gmail.com>
2024-08-29 13:39:01 +0200

    added somma array

---
+++ es.14 js/index.js
@@ -0,0 +1,12 @@
+let listnumber = [1, 5, 10, 15];
+var somma = 0;
+
+for (var i = 0; i < listnumber.length; i++) {
+  somma += listnumber[i];
+}
+console.log("La somma degli elementi dell'array è: " + somma);
+
+
+
+
+
