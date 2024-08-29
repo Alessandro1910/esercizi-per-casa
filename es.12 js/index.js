e53382a6105d2cea4837283faa005d3806bb8f6d
Alessandro1910 <alessandroturacci1910@gmail.com>
2024-08-29 12:17:32 +0200

    added swithc case

---
+++ es.12 js/index.js
@@ -0,0 +1,15 @@
+let number = 2;
+
+switch (number) {
+    case 1:
+        console.log("uno")
+        break
+    case 2:
+        console.log("due")
+        break
+    case 3:
+        console.log("tre")
+        break
+    default:
+        console.log("numero non valido")
+}
