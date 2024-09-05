cdf57d089dec46f0f5ba531b0b269a848924b947
Alessandro1910 <alessandroturacci1910@gmail.com>
2024-09-05 17:39:23 +0200

    add copyarray

---
+++ es.copia di un array js/index.js
@@ -0,0 +1,7 @@
+const number = [1, 30, 40, 50];
+
+console.log(number);
+
+const copynumber = [...number];
+
+console.log(copynumber);
---
+++ es.dichiara oggetto proprietà mancanti js/index.js
@@ -0,0 +1,11 @@
+let oggetto = {
+    marca: "Mercedes",
+    modello: "Suv",
+    undefined: "indirizzo",
+    undefined: "regione",
+}
+
+
+let{marca, modello, via, campania} = oggetto
+
+console.log(oggetto)
