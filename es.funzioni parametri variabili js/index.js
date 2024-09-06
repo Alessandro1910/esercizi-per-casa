ef1805460836d2308e166e44b6fc957fc63cf0e7
Alessandro1910 <alessandroturacci1910@gmail.com>
2024-09-06 15:16:55 +0200

    add calcolatore

---
+++ es.funzioni parametri variabili js/index.js
@@ -0,0 +1,11 @@
+ function unisci(cifra1, cifra2, cifra3) {
+    return cifra1 + cifra2 + cifra3;
+}
+
+console.log(unisci(4, 10, 50))
+
+/*function somma( num1, num2) {
+  return num1 + num2;
+}
+
+console.log(somma(3, 6))*/
