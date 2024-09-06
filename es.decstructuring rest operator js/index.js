86f78399ac27a16c148cd8c2ac47d5e5c745ddef
Alessandro1910 <alessandroturacci1910@gmail.com>
2024-09-06 16:09:24 +0200

    add decstr and rest operator

---
+++ es.decstructuring rest operator js/index.js
@@ -0,0 +1,20 @@
+const number = [10, 15, 25, 40]
+
+const [mario, alessandro] = number
+
+console.log(mario, alessandro)
+
+const newArray = [25, 40]
+function assegnazione(...altrivalori) {
+    return altrivalori
+}
+console.log(assegnazione(25, 40))
+
+/*const nuovoarray = [10, 15, 25, 40]
+
+function number(valore1, valore2, valore3, valore4){
+    return (valore3, valore4)
+}
+
+console.log(number)*/
+
