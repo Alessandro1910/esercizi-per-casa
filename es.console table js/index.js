15e41510e2d6044a9e4960c614bcf299038942f2
Alessandro1910 <alessandroturacci1910@gmail.com>
2024-09-06 22:11:54 +0200

    add table and group

---
+++ es.console table js/index.js
@@ -0,0 +1,15 @@
+
+
+console.table(["scarpe", "maglia", "jeans"]);
+
+
+
+console.log("gruppo 1");
+console.group();
+console.log("gruppo 2");
+console.group();
+console.log("gruppo 3");
+console.group();
+console.groupEnd("fine gruppo");
+
+
