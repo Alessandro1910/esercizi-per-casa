4f88f63b2e426c317529d23554bc2831a6c5c9fe
Alessandro1910 <alessandroturacci1910@gmail.com>
2024-09-06 17:45:11 +0200

    add Jsonstringify

---
+++ es.stringa json js/index.js
@@ -0,0 +1,13 @@
+const calciatori = {
+    titolo: "Lista",
+    elenco:[
+        "mbappe",
+        "zaccagni",
+        "Kean",
+        "Abrahm",
+        "Tammy"
+    ]
+};
+
+const calciatoriJson = JSON.stringify(calciatori)
+console.log(calciatoriJson)
