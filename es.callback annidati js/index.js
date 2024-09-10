924ba17dae7ab8dee90bc5498a43d19ee6c09c79
Alessandro1910 <alessandroturacci1910@gmail.com>
2024-09-10 13:13:33 +0200

    add funzione in una funzione

--- es.callback annidati js/index.js
+++ es.callback annidati js/index.js
@@ -1,12 +1,12 @@
 function completaOperazione(callback) {
     setTimeout(function() {
-        console.log("Prima operazione");
+        console.log("Prima operazione callback");
         callback();
     }, 5000);
 }
 
 function secondaFunzione() {
-    console.log("Callback seconda operazione");
+    console.log("Callback seconda operazione di funzione in una funzione");
 }
 
 completaOperazione(secondaFunzione);
