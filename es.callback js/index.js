be5556a3b6ecf7dbb105bc533b1a73031073b7f5
Alessandro1910 <alessandroturacci1910@gmail.com>
2024-09-10 11:43:11 +0200

    add finally

---
+++ es.callback js/index.js
@@ -0,0 +1,6 @@
+function valoreSomma(valore1, valore2){
+    return (valore1 + valore2)
+}
+
+let newvalore = valoreSomma(2, 5)
+console.log(newvalore)
---
+++ es.uso di finally js/index.js
@@ -0,0 +1,7 @@
+try{
+    console.lo("il messaggio e pronto per essere inviato")
+} catch {
+    console.log("error")
+} finally {
+    console.log("operazione completata con successo")
+}
