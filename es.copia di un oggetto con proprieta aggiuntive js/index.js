1bd84aa98d2a71aaf9017a9f038b203ccc308bc6
Alessandro1910 <alessandroturacci1910@gmail.com>
2024-09-11 13:52:05 +0200

    copia di un oggetto con proprieta aggiunte

---
+++ es.copia di un oggetto con proprieta aggiuntive js/index.js
@@ -0,0 +1,12 @@
+{
+const student1 = {
+    nome: 'aldo',
+    cognome: 'rossi',
+    eta: 25
+};
+console.log(student1)
+
+const student2 = {...student1, paese:'italia'};
+
+console.log(student2)
+}
