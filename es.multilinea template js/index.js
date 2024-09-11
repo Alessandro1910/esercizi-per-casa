9c3ec94bb30d0bdf0a17a943d0f3139e2f1dbf9b
Alessandro1910 <alessandroturacci1910@gmail.com>
2024-09-11 17:28:06 +0200

    multilinea usando template literals

--- es.multilinea template js/index.js
+++ es.multilinea template js/index.js
@@ -1,6 +1,20 @@
-let nome = 'Alessandro';
+/* let nome = 'Alessandro';
 let cognome = 'Turacci';
 let eta = 'ventiquattro anni';
 let citta = `Napoli ${nome + cognome + eta}`;
 
-console.log(citta)
+console.log(citta) */
+
+let nome = "alessandro";
+let cognome = "turacci";
+let eta = 23;
+let citta = "napoli";
+
+let unione = `
+nome: ${nome}
+cognome: ${cognome}
+eta: ${eta}
+citta: ${citta}
+`;
+
+console.log(unione)
