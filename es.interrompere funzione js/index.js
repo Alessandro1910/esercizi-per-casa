aa1fd320e5e882ea21fb0a9f7e1c005e63b296a9
Alessandro1910 <alessandroturacci1910@gmail.com>
2024-09-11 18:25:01 +0200

    clearInterval and setInterval

--- es.interrompere funzione js/index.js
+++ es.interrompere funzione js/index.js
@@ -1,13 +1,21 @@
 console.log("inizio conteggio ad intervallo")
 let timer = setInterval(function(){
     document.body.innerHTML += "iniziera un nuovo flusso di messaggi ogni secondo."
-}, 1000);
+}, 5000);
 clearInterval(timer);
 
-console.log("inizio dopo 3 secondi...")
+console.log("inizio dopo 5 secondi...")
 setTimeout(function() {
-    document.body.innerHTML += "riparti dopo 3 secondi."
-}, 3000);
+    document.body.innerHTML += "riparti dopo 5 secondi."
+}, 1000); 
+
+/*(function check() { 
+    console.log("Exists!"); 
+    return setInterval(check, 1000); 
+})();
+
+for (var i = 1; i < 99999; i++)
+    window.clearInterval(i);*/
 
   
 
