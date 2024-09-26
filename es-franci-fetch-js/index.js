c442f5a17524aede98ede3059b964a24b77e713b
Alessandro1910 <alessandroturacci1910@gmail.com>
2024-09-26 11:55:06 +0200

    esercitazione fetch

---
+++ es-franci-fetch-js/index.js
@@ -0,0 +1,33 @@
+const result = fetch("https://api.openbrewerydb.org/v1/breweries") // chiamata fetch al link e dichiarazione variabile
+function quandoLaFetchPronta(risposta) {
+    console.log(risposta)
+    if (risposta.ok == true) {
+        const responsejson = risposta.json();
+
+        console.log(responsejson)
+        responsejson.then(function (birrerie) {
+
+            console.log(birrerie)
+            for (let i = 0; i < birrerie.length; i++) {
+                const birreria = birrerie[i];
+                const nome = birreria.name
+                console.log(nome)
+
+                const li = document.createElement('li');
+
+                const ul = document.querySelector('ul');
+                ul.appendChild(li);
+                li.append(nome);
+            }
+        })
+
+    }
+
+}
+result.then(quandoLaFetchPronta)
+/* result.then(() => {  altro modo di dichiarare
+}) */
+
+// to do : refactorizzare il codice per renderlo piu leggibile (usare async e await e "=>" )
+
+
--- es-ripetizione-francesco-js/index.js
+++ es-ripetizione-francesco-js/index.js
@@ -1,7 +1,22 @@
-/* quando il pulsante viene cliccato diventa rosso */
+/* quando il pulsante viene cliccato diventa rosso 
 
 const bottone = document.querySelector('button');
 
 bottone.addEventListener('click', () => {
     bottone.classList.add('nuovobottone')
-});
+}); */
+
+fetch("https://miatech.it/platform/student/student-lessons?page=10") 
+.then(response => response.json ())
+.then (json => {
+    let listaPizze = "<table border = 1 >";
+    json.pizze.array.forEach(pizza => {
+        listaPizze += `<tr><td> ${pizza.nome} </td><td> ${pizza.descrizione} </td></tr>`;
+    });
+
+    listaPizze += "</table>"
+    document.body.innerHTML += listaPizze;
+})
+
+.catch(console.error)
+
