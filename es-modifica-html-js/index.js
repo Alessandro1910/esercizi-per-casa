aa44edfd13f435b5117a97143d3831c3062c0813
Alessandro1910 <alessandroturacci1910@gmail.com>
2024-09-26 23:32:27 +0200

    modifica HTML

---
+++ es-modifica-html-js/index.js
@@ -0,0 +1,9 @@
+function cliccami() {
+    var paragrafo = document.getElementById("mioParagrafo");
+
+    paragrafo.innerHTML = 'Paragrafo modificato tramite button <button> Bottone inserito con Inner </button>';
+}
+
+
+
+
