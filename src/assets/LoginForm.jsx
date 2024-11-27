8ce25c4c4b13444ca7a7aaf173c4825b8b608dcc
Alessandro1910 <alessandroturacci1910@gmail.com>
2024-11-27 17:27:43 +0100

    Alert submit esercizio numero 13

--- src/assets/LoginForm.jsx
+++ src/assets/LoginForm.jsx
@@ -20,18 +20,19 @@ const LoginForm = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        alert(`Username: ${username} Password: ${password}`);
     }
 
     return (
       <>
        <form onSubmit={handleSubmit}>
         <div>
-        <label htmlFor="">Username:{username}</label>
-        <input className="p-2" onChange={handleChangeUser} type="text" name="username" id="username" placeholder="enter username" />
+        <label htmlFor="">Username:</label>
+        <input className="p-2" onChange={handleChangeUser} value={username} type="text" name="username" id="username" placeholder="enter username" />
         </div>
         <div>
-        <label htmlFor="">Password:{password}</label>
-        <input className="p-2" onChange={handleChangePass} type="text" name="password" id="password" placeholder="enter password"/>  
+        <label htmlFor="">Password:</label>
+        <input className="p-2" onChange={handleChangePass} value={password} type="password" name="password" id="password" placeholder="enter password"/>  
         </div>
         <button className="bg-gray-400 rounded" type="submit">Login</button>
        </form>
