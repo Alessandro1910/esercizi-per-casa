29fda29ec6ccec1958d08a6040c390b78f2a78a6
Alessandro1910 <alessandroturacci1910@gmail.com>
2024-11-29 18:06:01 +0100

    esercizio 15

--- es_15/src/assets/UncontrolledInput.jsx
+++ es_15/src/assets/UncontrolledInput.jsx
@@ -3,18 +3,13 @@ import React, { useRef } from 'react';
 const UncontrolledInput = () => {
   const inputRef = useRef(null); 
 
-  const handleButtonClick = () => {
-    
-    alert(`Il valore dell'input è: ${inputRef.current.value}`);
-  };
-
   return (
     <div>
       <label>
         Inserisci un testo:
         <input type="text" ref={inputRef} /> 
       </label>
-      <button onClick={handleButtonClick}>Mostra valore</button>
+      <button >Mostra valore</button>
     </div>
   );
 };
