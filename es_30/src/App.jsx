69ab33505aabd247fbba6cb864a872a45465117c
Alessandro1910 <alessandroturacci1910@gmail.com>
2024-12-04 12:25:00 +0100

    esercizio rotte numero 30

---
+++ es_30/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Home from './assets/Home';
+import About from './assets/About';
+import{ Link, Route, Routes } from "react-router-dom";
+
+
+
+const App = () => {
+  return (
+   <>
+   <nav>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+   </nav>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/about" element={<About/>}/>
+  </Routes>
+   </>
+  );
+};
+
+export default App;
+
+
+
