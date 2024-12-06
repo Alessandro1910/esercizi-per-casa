a6798f54c175d70eaa54816ab60ec7812771f5aa
Alessandro1910 <alessandroturacci1910@gmail.com>
2024-12-06 14:49:44 +0100

    esesrcizio numero 3

---
+++ es_3/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const myTodo = {
+    id: 1,
+    title: "Learn TypeScript",
+    completed: false
+};
+console.log(myTodo);
---
+++ es_3/index.ts
@@ -0,0 +1,9 @@
+import { Todo } from './type';
+
+const myTodo: Todo = {
+    id: 1,
+    title: "Learn TypeScript",
+    completed: false
+};
+
+console.log(myTodo);
