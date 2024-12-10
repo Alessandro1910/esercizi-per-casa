b27e5aca6287c47c6197f48234bf47df4a964e57
Alessandro1910 <alessandroturacci1910@gmail.com>
2024-12-10 23:38:07 +0100

    es 24

---
+++ es_24/src/index.ts
@@ -0,0 +1,19 @@
+import { User } from './User';
+import { Todo, TodoStatus } from './types';
+import { filterTodos } from './utils';
+
+// Creazione di alcuni Todo
+const todos: Todo[] = [
+    { id: 101, title: "Comprare il latte", completed: false, status: TodoStatus.Pending },
+    { id: 102, title: "Leggere un libro", completed: true, status: TodoStatus.Completed },
+    { id: 103, title: "Fare esercizio fisico", completed: false, status: TodoStatus.InProgress },
+    { id: 104, title: "Studiare TypeScript", completed: true, status: TodoStatus.Completed },
+];
+
+// Filtrare i Todo completati
+const completedTodos = filterTodos(todos, (todo) => todo.completed);
+console.log("Todo completati:", completedTodos);
+
+// Filtrare i Todo con status Pending
+const pendingTodos = filterTodos(todos, (todo) => todo.status === TodoStatus.Pending);
+console.log("Todo in sospeso:", pendingTodos);
